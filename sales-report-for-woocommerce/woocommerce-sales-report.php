45526e1ca895e05b4b998559766137ecef329abd
Sean Murphy <sean@iamseanmurphy.com>
2025-07-14 20:18:25 +0000

    3.6.2.1

--- sales-report-for-woocommerce/woocommerce-sales-report.php
+++ sales-report-for-woocommerce/woocommerce-sales-report.php
@@ -3,17 +3,17 @@
  * Plugin Name: Sales Report for WooCommerce
  * Plugin URI: https://wordpress.org/plugins/sales-report-for-woocommerce/
  * Description: Generates daily, weekly and monthly sales report
- * Version: 3.6.1
+ * Version: 3.6.2.1
  * Author: BeRocket
  * Requires at least: 5.0
- * Tested up to: 6.7
+ * Tested up to: 6.8
  * Requires PHP: 7.0
  * Requires Plugins: woocommerce
  * Author URI: https://berocket.com?utm_source=free_plugin&utm_medium=plugins&utm_campaign=sales_report
  * Text Domain: sales-report-for-woocommerce
  * Domain Path: /languages/
- * WC tested up to: 9.5
+ * WC tested up to: 10.0
  */
-define( "BeRocket_sales_report_version", '3.6.1' );
+define( "BeRocket_sales_report_version", '3.6.2.1' );
 define( "BeRocket_sales_report_file", __FILE__ );
 include_once('main.php');
